dc4c32170e43a8b286ecfdac61485b24de6ad26a
Elena Fernández Cañizares <elena.fernandez@bootcamp-upgrade.com>
2025-02-10 21:00:33 +0100

    primeras pruebas de figma

--- app/src/main/java/com/example/proyecto_last_shot/PaginaPrincipal.java
+++ app/src/main/java/com/example/proyecto_last_shot/PaginaPrincipal.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 
 public class PaginaPrincipal extends AppCompatActivity {
 
@@ -14,11 +13,11 @@ public class PaginaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.pagina_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_pagina_inicial);
+
+        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/53e4e592-9b5a-4a0c-93df-70fdf9f35566").into((ImageView) findViewById(R.id.rumfs3dbdl2));
+        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/5cfde65b-4c31-462d-9462-de0ddb6c19f8").into((ImageView) findViewById(R.id.rrfkyk5wjuh9));
+        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/38cabae6-f779-411d-91f7-083538423791").into((ImageView) findViewById(R.id.rbaxguoevnfp));
+
     }
 }
