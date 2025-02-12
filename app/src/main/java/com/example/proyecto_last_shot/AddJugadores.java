bad259b5256c81ada56e0769c88c00b56eaa538a
Elena Fernández Cañizares <elena.fernandez@bootcamp-upgrade.com>
2025-02-12 17:43:38 +0100

    primera estructura sin imagenes

--- app/src/main/java/com/example/proyecto_last_shot/AddJugadores.java
+++ app/src/main/java/com/example/proyecto_last_shot/AddJugadores.java
@@ -1,24 +1,64 @@
 package com.example.proyecto_last_shot;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AddJugadores extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.add_jugadores);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.pagina_add_jugadores);
+
+        // Buscar el ImageView por su ID
+        ImageView logo = findViewById(R.id.logo);
+        ImageView imagenPrincipal = findViewById(R.id.imagenPrincipal);
+        ImageView iconoOpcion2 = findViewById(R.id.iconoOpcion2);
+
+        // Cargar las imágenes en segundo plano
+        new DescargarImagen(logo).execute("https://via.placeholder.com/150");
+        new DescargarImagen(imagenPrincipal).execute("https://via.placeholder.com/300");
+        new DescargarImagen(iconoOpcion2).execute("https://via.placeholder.com/100");
+    }
+
+    // Clase interna para descargar imágenes en segundo plano
+    private static class DescargarImagen extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DescargarImagen(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap bitmap = null;
+            try {
+                // Conectar a la URL y obtener la imagen
+                URL imageUrl = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) imageUrl.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            if (result != null) {
+                imageView.setImageBitmap(result);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/proyecto_last_shot/PaginaPrincipal.java
+++ app/src/main/java/com/example/proyecto_last_shot/PaginaPrincipal.java
@@ -1,8 +1,6 @@
 package com.example.proyecto_last_shot;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
@@ -12,12 +10,19 @@ public class PaginaPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pagina_inicial);
+        setContentView(R.layout.activity_pagina_inicial); // Asegúrate de que este archivo XML sea el correcto
 
-        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/53e4e592-9b5a-4a0c-93df-70fdf9f35566").into((ImageView) findViewById(R.id.rumfs3dbdl2));
-        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/5cfde65b-4c31-462d-9462-de0ddb6c19f8").into((ImageView) findViewById(R.id.rrfkyk5wjuh9));
-        Glide.with(this).load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/38cabae6-f779-411d-91f7-083538423791").into((ImageView) findViewById(R.id.rbaxguoevnfp));
+        // Cargar imágenes usando Glide
+        Glide.with(this)
+                .load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/53e4e592-9b5a-4a0c-93df-70fdf9f35566")
+                .into((ImageView) findViewById(R.id.menu));
 
+        Glide.with(this)
+                .load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/5cfde65b-4c31-462d-9462-de0ddb6c19f8")
+                .into((ImageView) findViewById(R.id.logo));
+
+        Glide.with(this)
+                .load("https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/38cabae6-f779-411d-91f7-083538423791")
+                .into((ImageView) findViewById(R.id.cerveza));
     }
 }
