57e91d8ca5711c8fba5d11d3f21640bc409ce365
Elena Fernández Cañizares <elena.fernandez@bootcamp-upgrade.com>
2025-02-20 18:02:11 +0100

    manifest

--- app/src/main/java/com/example/proyecto_last_shot/RetoActivity.java
+++ app/src/main/java/com/example/proyecto_last_shot/RetoActivity.java
@@ -1,4 +1,15 @@
 package com.example.proyecto_last_shot;
 
-public class RetoActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RetoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.reto);
+    }
 }
